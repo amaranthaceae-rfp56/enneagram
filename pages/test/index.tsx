32124064898a06a5519c0a9eacc260cf14e557c3
Carlos Morales <65197354+cmoralespv@users.noreply.github.com>
2022-01-19 20:43:34 -0700

    testing questions page

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,7 +1,8 @@
-import React, { useState} from 'react';
-import Image from 'next/image';
-import Button from '@mui/material/Button';
-import logo from '../components/assets/eneagrama.png';
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Button from '@mui/material/Button'
+import logo from '../../assets/eneagrama.png'
 
 import { imageConfigDefault } from 'next/dist/server/image-config';
 
@@ -17,7 +18,11 @@ const Start = () => {
           <Image className="enneagram-logo" src={ logo } alt="logo"/>
             </header>
               <div className="login-buttons-container">
-                <Button variant="contained">Start Test</Button>;
+                <Link href="/test/[question]" as={ `/test/4` } passHref>
+                  <Button
+                    variant="contained">Start Test
+                  </Button>
+                </Link>
           </div>
        </>
    );
