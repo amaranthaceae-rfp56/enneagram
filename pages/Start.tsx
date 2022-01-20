617af95e1ab299c267126f29bf1cf2570df90eaa
Carlos Morales <65197354+cmoralespv@users.noreply.github.com>
2022-01-19 18:41:48 -0700

    testing start page implementation

---
+++ pages/Start.tsx
@@ -0,0 +1,26 @@
+import React, { useState} from 'react';
+import Image from 'next/image';
+import Button from '@mui/material/Button';
+import logo from '../components/assets/eneagrama.png';
+
+import { imageConfigDefault } from 'next/dist/server/image-config';
+
+type StartProps = {
+};
+
+const Start = () => {
+
+  return (
+    <>
+      <header className="enneagram-main">
+        <div className="title">E9 ENNEAGRAM TEST</div>
+          <Image className="enneagram-logo" src={ logo } alt="logo"/>
+            </header>
+              <div className="login-buttons-container">
+                <Button variant="contained">Start Test</Button>;
+          </div>
+       </>
+   );
+};
+
+export default Start;
